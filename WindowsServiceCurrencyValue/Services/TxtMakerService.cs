268ec6ad871f242cb2272454942fd6f4627879f0
ALCM-bit <felipealcmagalhaes@gmail.com>
2023-06-08 19:02:37 -0300

    Error Handling for write txt file

--- WindowsServiceCurrencyValue/Services/TxtMakerService.cs
+++ WindowsServiceCurrencyValue/Services/TxtMakerService.cs
@@ -9,39 +9,59 @@ using WindowsServiceCurrencyValue.Models;
 
 namespace WindowsServiceCurrencyValue.Services
 {
-    public class TxtMakerService: ITxtMaker
+    public class TxtMakerService : ITxtMaker
     {
 
         public void WriteData(List<DataFormat> data)
         {
-            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
-            if (!Directory.Exists(path))
+            try
             {
-                Directory.CreateDirectory(path);
-            }
-            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs", $"ServiceLog_{DateTime.Now.Date.ToShortDateString().Replace('/', '-')}.txt");
-            using (StreamWriter writer = new StreamWriter(filePath, false))
-            {
-                string important = "Caso os valores apareçam como 0 significa que não há nenhuma atualização no dia de hoje." +
-                    "Caso a informação persistir entre em contato com o suporte";
-                writer.WriteLine(important);
-                writer.WriteLine("-----------------------------------------------------------------------------");
-                writer.WriteLine("Ultima pesquisa: " + DateTime.Now.ToString());
-                string header = "Simbolo | Nome Formatado | Valor Compra | Valor Venda | Data e Hora da Cotação";
-                writer.WriteLine(header);
-                foreach (DataFormat item in data)
+                string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs", $"ServiceLog_{DateTime.Now.Date.ToShortDateString().Replace('/', '-')}.txt");
+                if (!Directory.Exists(path))
                 {
-                    string line = $"{item.simbolo} - {item.nomeFormatado}  | {item.cotacaoCompra}" +
-                        $" | {item.cotacaoVenda} | {item.dataHoraCotacao}";
-                    writer.WriteLine(line);
-                    writer.WriteLine("");
+                    Directory.CreateDirectory(path);
                 }
+                using (StreamWriter writer = new StreamWriter(filePath, false))
+                {
+                    string important = "Caso os valores apareçam como 0 significa que não há nenhuma atualização no dia de hoje." +
+                        "Caso a informação persistir entre em contato com o suporte";
+                    writer.WriteLine(important);
+                    writer.WriteLine("-----------------------------------------------------------------------------");
+                    writer.WriteLine("Ultima pesquisa: " + DateTime.Now.ToString());
+                    string header = "Simbolo | Nome Formatado | Valor Compra | Valor Venda | Data e Hora da Cotação";
+                    writer.WriteLine(header);
+                    foreach (DataFormat item in data)
+                    {
+                        string line = $"{item.simbolo} - {item.nomeFormatado}  | {item.cotacaoCompra}" +
+                            $" | {item.cotacaoVenda} | {item.dataHoraCotacao}";
+                        writer.WriteLine(line);
+                        writer.WriteLine("");
+                    }
+                }
+
             }
+            catch (Exception ex)
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + "\\LogsErrors";
+                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs", $"ServiceLog_{DateTime.Now.Date.ToShortDateString().Replace('/', '-')} Error.txt");
+                using (StreamWriter writer = new StreamWriter(filePath, false))
+                {
+                    writer.WriteLine("O programa parou de funcionar em: " + DateTime.Now.ToString());
+                    writer.WriteLine("");
+                    writer.WriteLine("--------------------------------------");
+                    writer.WriteLine("");
+                    writer.WriteLine("PROBLEMA: ");
+                    writer.Write(ex.Message);
+                }
+
+            }
+
         }
 
         public void WriteStopMessage()
         {
-            string FilePath = $"C:\\ws-workspace\\Desafio_MXM\\CoinValueService\\{DateTime.Now.Date.ToShortDateString()} Stoped.txt";
+            string FilePath = $"C:\\ws-workspace\\Desafio_MXM\\CoinValueService\\{DateTime.Now.Date.ToShortDateString().Replace('/', '-')} Stoped.txt";
             using (StreamWriter writer = new StreamWriter(FilePath, false))
             {
                 writer.WriteLine("O serviço parou de funcionar" + DateTime.Now.ToString());
