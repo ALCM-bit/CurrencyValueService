8afbe6e341907512b63a3bcd2ddf06b5f053c56f
Felipe Augusto L. C. Magalhães <felipealcmagalhaes@gmail.com>
2023-06-10 14:17:24 -0300

    Removed test console app

--- CoinValue/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using CoinValue.Services;
-
-RequestCentralBankAPIService request = new RequestCentralBankAPIService();
-
-var result = await request.GetAllCurenci();
-
-TxtMaker maker = new TxtMaker();
-
-maker.WriteData(result);
